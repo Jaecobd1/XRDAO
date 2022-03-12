cfadb689e1346a3ae0af4d14e0d8d6587192171c
Jaecobd1 <jaecob.d1@gmail.com>
2022-03-12 13:28:53 -0500

    fix

--- Components/Functions/GetSwapToken2Estimate.tsx
+++ Components/Functions/GetSwapToken2Estimate.tsx
@@ -1,10 +1,10 @@
 import useWeb3ExecuteFunction from 'react-moralis'
-import {abi, CONTRACT_ADDRESS} from '../constants'
+// import {abi, CONTRACT_ADDRESS} from '../constants'
 export default function getSwapToken2Estimate(amountTo){
 
 const { data, error, fetch, isFetching, isLoading } = useWeb3ExecuteFunction({
-    abi: abi,
-    contractAddress: CONTRACT_ADDRESS,
+    // abi: abi,
+    // contractAddress: CONTRACT_ADDRESS,
     functionName: "getSwapToken2EstimateGivenToken1",
     params: {
         _amountToken1: amountTo
