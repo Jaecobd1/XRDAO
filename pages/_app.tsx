d975e3ac2185264fecaa2365f6fc3fd7140e58b1
Jaecobd1 <jaecob.d1@gmail.com>
2022-03-12 20:40:48 -0500

    website update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import Nav from '../Components/nav'
 import Layout from '../Components/Layout'
 import { motion } from "framer-motion"
 import {MoralisProvider} from 'react-moralis'
-
+import Footer from '../Components/nav/Footer'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -14,11 +14,14 @@ function MyApp({ Component, pageProps }: AppProps) {
     <MoralisProvider 
     appId={process.env.NEXT_PUBLIC_APP_ID ?? "AppId Undefined Fallback"} 
     serverUrl={process.env.NEXT_PUBLIC_SERVER_URL ?? "ServerUrl Undefined Fallback"}>
-        <Layout>
-          
+        <div className="z-40 w-screen sticky top-0 rounded-b-xl drop-shadow-lg text-white" >
+            <Nav />
+        </div>
             <Component {...pageProps} />
            
-        </Layout>
+         <div className="text-left text-slate-300 w-screen bottom-10 bg-grey ">
+           <Footer />
+        </div>
   
         </MoralisProvider>
          
